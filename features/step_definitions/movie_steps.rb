88a9a7b310a0732c85489772afe4b4b66bb8babc
saasbook <saasbook@saasbook.(none)>
2013-08-14 22:38:49 -0700

    hw3-partc

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -29,3 +29,7 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
 end
+
+Then /I should (not\s)?see movies with the ratings: (.*)/ do |see, rating_list|
+  
+end 
